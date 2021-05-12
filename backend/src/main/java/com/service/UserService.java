2bfdc5faa97cb56adaa031d949e8207f587fb262
Stian Mogen <stianfmo@stud.ntnu.no>
2021-05-12 13:31:19 +0200

    user service

--- backend/src/main/java/com/service/UserService.java
+++ backend/src/main/java/com/service/UserService.java
@@ -1,4 +1,14 @@
 package com.service;
 
+import com.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public interface UserService {
+    UserDto getUserDtoByEmail(String email);
+    List<UserDto> getAllUsersDto();
+    UserDto getUserByUUID(UUID userId);
 }
