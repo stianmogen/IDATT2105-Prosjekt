9aeb79e300512af253dbdffaabec8a77abbef4b4
Lars <larsbost@stud.ntnu.no>
2021-05-13 10:37:52 +0200

    started with token

--- backend/src/main/java/com/security/SecurityConfig.java
+++ backend/src/main/java/com/security/SecurityConfig.java
@@ -1,20 +1,109 @@
 package com.security;
 
+import com.security.filter.JwtTokenFilter;
+import com.service.UserDetailsServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.servlet.http.HttpServletResponse;
 
 @AllArgsConstructor
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JwtTokenFilter jwtTokenFilter;
+
     @Autowired
     private UserDetailsService userDetailsService;
 
-     
-    
+
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http = http.cors().and().csrf().disable();
+        http = http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and();
+        http = http
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, ex) -> {
+                            response.sendError(
+                                    HttpServletResponse.SC_UNAUTHORIZED,
+                                    ex.getMessage()
+                            );
+                        }
+                )
+                .and();
+
+
+        // TODO her må vi sette permissions til endepunkter etterhvert.
+        http.authorizeRequests()
+                // Our public endpoints
+                .antMatchers("/api/public/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/user/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/user/search").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/room/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/room/search").permitAll()
+                // Our private endpoints
+                .anyRequest().authenticated();
+
+
+        // Add JWT token filter
+        http.addFilterBefore(
+                jwtTokenFilter,
+                UsernamePasswordAuthenticationFilter.class
+        );
+
+
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService())
+                .passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    /**
+     * This sets up the configuration for Cross-Origin Resource Sharing (CORS)
+     *
+     * Note:
+     * Cors is not a protection agains CSRF attacks
+     * Poor cors configuration opens for cross-domain based attacks
+     *
+     * @return the configuration source
+     */
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+
 }
