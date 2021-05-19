efe094fea19c5da56b376d189226d9498248932e
NIcolay Schiøll-Johansen <nicolayschiolljohansen@gmail.com>
2021-05-19 19:54:49 +0200

    Fixed basic styling

--- frontend/src/components/inputs/SearchBar.tsx
+++ frontend/src/components/inputs/SearchBar.tsx
@@ -2,7 +2,9 @@ import { useState } from 'react';
 
 // Material UI Components
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Button, IconButton, Collapse, Divider } from '@material-ui/core';
+import { Typography, Button, IconButton, Collapse, Divider, MenuItem } from '@material-ui/core';
+import LocalizationProvider from '@material-ui/lab/LocalizationProvider/LocalizationProvider';
+import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
 
 // Icons
 import SearchIcon from '@material-ui/icons/SearchRounded';
@@ -10,7 +12,10 @@ import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 // Project components
 import Paper from 'components/layout/Paper';
-
+import Bool from './Bool';
+import { useForm } from 'react-hook-form';
+import DatePicker from 'components/inputs/DatePicker';
+import Select from 'components/inputs/Select';
 const useStyles = makeStyles((theme) => ({
   paper: {
     borderRadius: 20,
@@ -61,14 +66,42 @@ const useStyles = makeStyles((theme) => ({
   icon: {
     marginLeft: 'auto',
   },
+  buildingSelector: {
+    display: 'flex',
+    justifyContent: 'left',
+    alignItems: 'center',
+  },
 }));
+
+type FormValues = {
+  building1: boolean;
+  building2: boolean;
+  building3: boolean;
+  date: Date;
+  startTime: Date;
+  endTime: Date;
+  amount: number;
+};
+
+const SORT_OPTIONS = [
+  { name: '1', key: '1' },
+  { name: '2', key: '2' },
+  { name: '3', key: '3' },
+  { name: '4', key: '4' },
+  { name: '5', key: '5' },
+  { name: '6', key: '6' },
+  { name: '7', key: '7' },
+  { name: '8', key: '8' },
+  { name: '9', key: '9' },
+  { name: '10', key: '10' },
+];
 const SearchBar = () => {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
   const [open2, setOpen2] = useState(false);
   const [open3, setOpen3] = useState(false);
   const [open4, setOpen4] = useState(false);
-
+  const { control, formState } = useForm<FormValues>();
   const closeAll = (id: number) => {
     if (setOpen && id !== 1) {
       setOpen(false);
@@ -102,6 +135,9 @@ const SearchBar = () => {
       setOpen4((prev) => !prev);
     }
   };
+
+  //TODO: Make mobile version
+
   return (
     <Paper className={classes.paper} noPadding>
       <form>
@@ -109,7 +145,6 @@ const SearchBar = () => {
           <Button aria-label='menu' className={classes.iconButton} fullWidth onClick={() => handleMenu('1')} variant='text'>
             <div className={classes.selected}>
               <Typography variant='h5'>Select Building</Typography>
-              <Typography variant='h4'>All Buildings</Typography>
             </div>
             <div className={classes.icon}>
               <ArrowDropDownIcon />
@@ -119,7 +154,6 @@ const SearchBar = () => {
           <Button aria-label='menu' className={classes.iconButton} fullWidth onClick={() => handleMenu('2')} variant='text'>
             <div className={classes.selected}>
               <Typography variant='h5'>Select Date</Typography>
-              <Typography variant='h4'>Any Date</Typography>
             </div>
             <div className={classes.icon}>
               <ArrowDropDownIcon />
@@ -129,7 +163,6 @@ const SearchBar = () => {
           <Button aria-label='menu' className={classes.iconButton} fullWidth onClick={() => handleMenu('3')} variant='text'>
             <div className={classes.selected}>
               <Typography variant='h5'>Select Time</Typography>
-              <Typography variant='h4'>0 Hours</Typography>
             </div>
             <div className={classes.icon}>
               <ArrowDropDownIcon />
@@ -139,7 +172,6 @@ const SearchBar = () => {
           <Button aria-label='menu' className={classes.iconButton} fullWidth onClick={() => handleMenu('4')} variant='text'>
             <div className={classes.selected}>
               <Typography variant='h5'>Select Amount</Typography>
-              <Typography variant='h4'>0 Spots</Typography>
             </div>
             <div className={classes.icon}>
               <ArrowDropDownIcon />
@@ -153,25 +185,49 @@ const SearchBar = () => {
         <Collapse in={open}>
           <Divider />
           <div className={classes.filterPaper} id='1'>
-            <Typography variant='h3'>Meny 1</Typography>
+            <div className={classes.buildingSelector}>
+              <Bool control={control} formState={formState} name='building1' type='checkbox' />
+              <Typography variant='h4'>Building 1</Typography>
+            </div>
+            <div className={classes.buildingSelector}>
+              <Bool control={control} formState={formState} name='building2' type='checkbox' />
+              <Typography variant='h4'>Building 2</Typography>
+            </div>
+            <div className={classes.buildingSelector}>
+              <Bool control={control} formState={formState} name='building3' type='checkbox' />
+              <Typography variant='h4'>Building 3</Typography>
+            </div>
           </div>
         </Collapse>
         <Collapse in={open2}>
           <Divider />
           <div className={classes.filterPaper} id='2'>
-            <Typography variant='h3'>Meny 2</Typography>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker control={control} formState={formState} fullWidth label='Date' margin='normal' name='date' type='date' />
+            </LocalizationProvider>
           </div>
         </Collapse>
         <Collapse in={open3}>
           <Divider />
           <div className={classes.filterPaper} id='2'>
             <Typography variant='h3'>Meny 3</Typography>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker control={control} formState={formState} fullWidth label='From' margin='normal' name='startTime' type='time' />
+              <DatePicker control={control} formState={formState} fullWidth label='To' margin='normal' name='endTime' type='time' />
+            </LocalizationProvider>
           </div>
         </Collapse>
         <Collapse in={open4}>
           <Divider />
           <div className={classes.filterPaper} id='2'>
             <Typography variant='h3'>Meny 4</Typography>
+            <Select control={control} defaultValue={SORT_OPTIONS[0].key} formState={formState} label='Amount' margin='dense' name='amount'>
+              {SORT_OPTIONS.map((value) => (
+                <MenuItem key={value.key} value={value.key}>
+                  {value.name}
+                </MenuItem>
+              ))}
+            </Select>
           </div>
         </Collapse>
       </form>
--- frontend/src/components/inputs/Select.tsx
+++ frontend/src/components/inputs/Select.tsx
@@ -2,13 +2,14 @@ import FormControl, { FormControlProps } from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import InputLabel from '@material-ui/core/InputLabel';
 import MuiSelect from '@material-ui/core/Select';
-import { Controller, RegisterOptions, UseFormReturn } from 'react-hook-form';
+import { Control, Controller, RegisterOptions, UseFormReturn } from 'react-hook-form';
 
 export type IProps = FormControlProps &
-  Pick<UseFormReturn, 'formState' | 'control'> & {
+  Pick<UseFormReturn, 'formState'> & {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    control: Control<any>;
     name: string;
     rules?: RegisterOptions;
-  } & {
     children: React.ReactNode;
     label: string;
     defaultValue?: string;
@@ -17,7 +18,7 @@ export type IProps = FormControlProps &
 const Select = ({ name, label, control, formState, rules = {}, defaultValue = '', children, ...props }: IProps) => {
   const labelId = `${name}-label`;
   return (
-    <FormControl fullWidth {...props}>
+    <FormControl fullWidth margin='normal' variant='outlined' {...props}>
       <InputLabel id={labelId} required={Boolean(formState.errors[name])}>
         {label}
       </InputLabel>
