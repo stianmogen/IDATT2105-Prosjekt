390f2b84959f074967ac68885226c900f5121091
Stian Mogen <stianfmo@stud.ntnu.no>
2021-05-21 19:01:22 +0200

    reservation view works

--- frontend/src/containers/Bookings/components/MyReservations.tsx
+++ frontend/src/containers/Bookings/components/MyReservations.tsx
@@ -1,13 +1,20 @@
-import { useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import { useMyReservatedSections } from 'hooks/Sections';
 
 // Material UI
 import { makeStyles } from '@material-ui/core/styles';
+import { Button, ButtonGroup, Collapse, Hidden, TextField, MenuItem } from '@material-ui/core';
+
+// Icons
+import FutureIcon from '@material-ui/icons/FastForwardRounded';
+import HistoryIcon from '@material-ui/icons/HistoryRounded';
+import LikedIcon from '@material-ui/icons/FavoriteRounded';
 
 // Project components
 import Pagination from 'components/layout/Pagination';
 import NotFoundIndicator from 'components/miscellaneous/NotFoundIndicator';
 import SectionCard from 'components/layout/SectionCard';
+import ReservationCard from 'components/layout/ReservationCard';
 
 const useStyles = makeStyles((theme) => ({
   menu: {
@@ -41,9 +48,22 @@ export type MyReservationsProps = {
   userId?: string;
 };
 
+type View = 'list' | 'calendar' | 'map';
+type Type = 'future' | 'past' | 'favourites';
+
 const MyReservations = ({ userId }: MyReservationsProps) => {
   const classes = useStyles();
-
+  const [type, setType] = useState<Type>('future');
+  const [view, setView] = useState<View>('list');
+  /* const filters = useMemo(
+    () =>
+      type === 'favourites'
+        ? {}
+        : {
+            [type === 'past' ? 'registrationStartDateBefore' : 'registrationStartDateAfter']: new Date().toISOString(),
+          },
+    [type],
+  );*/
   const useHook = useMyReservatedSections;
   const { data, error, hasNextPage, fetchNextPage, isFetching } = useHook(userId);
   const reservations = useMemo(() => (data !== undefined ? data.pages.map((page) => page.content).flat(1) : []), [data]);
@@ -51,14 +71,17 @@ const MyReservations = ({ userId }: MyReservationsProps) => {
 
   return (
     <>
-      <Pagination fullWidth hasNextPage={hasNextPage} isLoading={isFetching} nextPage={() => fetchNextPage()}>
-        {isEmpty && <NotFoundIndicator header={error?.message || 'Fant ingen reservasjoner'} />}
-        <div className={classes.list}>
-          {reservations.map((reservation) => (
-            <SectionCard fullHeight key={reservation.id} section={reservation} />
-          ))}
-        </div>
-      </Pagination>
+      <div className={classes.menu}></div>
+      <Collapse in={view === 'list'}>
+        <Pagination fullWidth hasNextPage={hasNextPage} isLoading={isFetching} nextPage={() => fetchNextPage()}>
+          {isEmpty && <NotFoundIndicator header={error?.message || 'Fant ingen reservasjoner'} />}
+          <div className={classes.list}>
+            {reservations.map((reservation) => (
+              <ReservationCard fullHeight key={reservation.id} reservation={reservation} />
+            ))}
+          </div>
+        </Pagination>
+      </Collapse>
     </>
   );
 };
